89f0a07e0e3c913b3c30cb0fdc7b6a219a3149cc
Luis Juarez <luisisthejuarez@gmail.com>
2021-11-05 23:47:12 -0400

    wiring up redux

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import {FetchPosts} from '../actions';
 
 class PostList extends React.Component {
+    componentDidMount() {
+        this.props.FetchPosts();
+    }
+
     render() {
         return (<div>PostList</div>)
     }
 }
 
-export default PostList;
+export default connect(null, { FetchPosts})(PostList);
